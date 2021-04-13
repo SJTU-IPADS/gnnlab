fb35cb5dec00b9c4df51f1eb19ee3b261106869a
Jianbang Yang <yangjianbang112@gmail.com>
2021-04-13 18:16:04 +0800

    add fast graph library

---
+++ example/dgl/fastgraph/__init__.py
@@ -0,0 +1,3 @@
+from .papers100M import Papers100M
+
+__all__ = ['Papers100M']
---
+++ example/dgl/fastgraph/setup.py
@@ -0,0 +1,8 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='fastgraph',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Fast graph dataset loading tools',
+)
