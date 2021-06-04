2871832d0cb1ffbcc08e28f4c24bc958f08f8ec2
Jianbang Yang <yangjianbang112@gmail.com>
2021-06-04 10:26:25 +0800

    add mlock test

--- tests/memcpy_test.cc
+++ tests/memcpy_test.cc
@@ -1,5 +1,6 @@
 #include <cuda_runtime.h>
 #include <gtest/gtest.h>
+#include <sys/mman.h>
 
 #include <cstdlib>
 
@@ -10,10 +11,12 @@ int n_iters = 10;
 size_t copy_nbytes = 500ull * 1024 * 1024;
 
 TEST(MemcpyTest, HostMalloc) {
-  double pinned_time = 0, time = 0;
+  double pinned_time = 0, unpinned_time = 0, mlock_time;
 
-  void *host_pinned_data, *host_unppined_data, *dev_data;
+  void *host_pinned_data, *host_unppined_data, *host_mlock_data, *dev_data;
   host_unppined_data = malloc(copy_nbytes);
+  host_mlock_data = malloc(copy_nbytes);
+  mlock(host_mlock_data, copy_nbytes);
   CUDA_CALL(
       cudaHostAlloc(&host_pinned_data, copy_nbytes, cudaHostAllocDefault));
   CUDA_CALL(cudaMalloc(&dev_data, copy_nbytes));
@@ -27,13 +30,21 @@ TEST(MemcpyTest, HostMalloc) {
     Timer t1;
     CUDA_CALL(cudaMemcpy(dev_data, host_unppined_data, copy_nbytes,
                          cudaMemcpyHostToDevice));
-    time += t1.Passed();
+    unpinned_time += t1.Passed();
+
+    Timer t2;
+    CUDA_CALL(cudaMemcpy(dev_data, host_mlock_data, copy_nbytes,
+                         cudaMemcpyHostToDevice));
+    mlock_time += t2.Passed();
   }
 
   CUDA_CALL(cudaFreeHost(host_pinned_data));
   CUDA_CALL(cudaFree(dev_data));
+  munlock(host_mlock_data, copy_nbytes);
+  free(host_mlock_data);
   free(host_unppined_data);
 
   LOG << "pinned: " << pinned_time / n_iters
-      << " | unpinned: " << time / n_iters << "\n";
+      << " | unpinned: " << unpinned_time / n_iters << " | mlock"
+      << mlock_time / n_iters << "\n";
 }
