bcfd02a2eb02a4ca970e1a81d538e1992936a454
sxn <xiaoniu.sxn@outlook.com>
2021-08-11 17:05:23 +0800

    a convenient runner

--- example/runner.sxn.py
+++ example/runner.sxn.py
@@ -3,8 +3,13 @@ import datetime
 from enum import Enum
 
 LOG_DIR='run-logs/logs_samgraph_' + datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+TMP_LOG_FILE = 'run-logs/tmp.log'
 
 root_path="/graph-learning/samgraph/"
+
+DO_MOCK=False
+TMP_LOG=True
+DURABLE_LOG=True
 
 class CachePolicy(Enum):
   cache_by_degree = 0
@@ -42,8 +47,7 @@ class RunConfig:
                pipeline:bool=False,
                max_sampling_jobs=10,
                max_copying_jobs=2,
-               fanout="25,10", # ignored when pinsage
-               arch:Arch=Arch.arch3,
+               arch:Arch=Arch.arch4,
                epoch:int=3, logdir:str=LOG_DIR):
     self.app           = app
     self.dataset       = dataset
@@ -52,7 +56,6 @@ class RunConfig:
     self.pipeline      = pipeline
     self.sample_job    = max_sampling_jobs
     self.copy_job      = max_copying_jobs
-    self.fanout        = fanout
     self.arch          = arch
     self.epoch         = epoch
     self.logdir        = logdir
@@ -68,8 +71,10 @@ class RunConfig:
     return []
 
   def form_cmd(self):
-    ## fixme: fanout not passed
-    cmd_line = f'python samgraph/train_{self.app.name}.py --arch {self.arch.name}'
+    cmd_line = ''
+    cmd_line += 'export SAMGRAPH_PROFILE_LEVEL=1; '
+    cmd_line += 'export SAMGRAPH_LOG_LEVEL=warn; '
+    cmd_line += f'python samgraph/train_{self.app.name}.py --arch {self.arch.name}'
     cmd_line += f' --max-sampling-jobs {self.sample_job}'
     cmd_line += f' --max-copying-jobs {self.copy_job}'
     if self.pipeline:
@@ -83,9 +88,12 @@ class RunConfig:
     
     cmd_line += f' --dataset-path {root_path}{str(self.dataset)}'
     cmd_line += f' --num-epoch {self.epoch}'
-    cmd_line += f' > \"{LOG_DIR}/'
-    cmd_line += '_'.join(['samgraph']+self.cache_log_name() + self.pipe_log_name()+[self.app.name, str(self.dataset)]) 
-    cmd_line += '.log\" 2>&1'
+    if TMP_LOG:
+      cmd_line += f' > \"{TMP_LOG_FILE}\" 2>&1'
+    elif DURABLE_LOG:
+      cmd_line += f' > \"{LOG_DIR}/'
+      cmd_line += '_'.join(['samgraph']+self.cache_log_name() + self.pipe_log_name()+[self.app.name, str(self.dataset)]) 
+      cmd_line += '.log\" 2>&1'
     return cmd_line
 
   def beauty(self):
@@ -97,78 +105,155 @@ class RunConfig:
       print(self.form_cmd())
     else:
       print(self.beauty())
+      os.system('mkdir -p {}'.format(LOG_DIR))
       os.system(self.form_cmd())
     pass
 
-conf_list = [
-  RunConfig(App.gcn,       Dataset.reddit     ),
-  RunConfig(App.gcn,       Dataset.products   ),
-  RunConfig(App.gcn,       Dataset.papers100M ),
-  RunConfig(App.gcn,       Dataset.friendster ),
+def run_in_list(conf_list : list):
+  for conf in conf_list:
+    conf : RunConfig
+    conf.run(DO_MOCK)
 
-  RunConfig(App.graphsage, Dataset.reddit     ),
-  RunConfig(App.graphsage, Dataset.products   ),
-  RunConfig(App.graphsage, Dataset.papers100M ),
-  RunConfig(App.graphsage, Dataset.friendster ),
+class ConfigList:
+  def __init__(self):
+    self.conf_list = [
+      RunConfig(App.gcn,       Dataset.reddit     ),
+      RunConfig(App.gcn,       Dataset.products   ),
+      RunConfig(App.gcn,       Dataset.papers100M ),
+      RunConfig(App.gcn,       Dataset.friendster ),
 
-  RunConfig(App.pinsage,   Dataset.reddit     ),
-  RunConfig(App.pinsage,   Dataset.products   ),
-  RunConfig(App.pinsage,   Dataset.papers100M ),
-  RunConfig(App.pinsage,   Dataset.friendster ),
-]
+      RunConfig(App.graphsage, Dataset.reddit     ),
+      RunConfig(App.graphsage, Dataset.products   ),
+      RunConfig(App.graphsage, Dataset.papers100M ),
+      RunConfig(App.graphsage, Dataset.friendster ),
 
-conf_list += [
-  RunConfig(App.gcn,       Dataset.reddit,      pipeline=True),
-  RunConfig(App.gcn,       Dataset.products,    pipeline=True),
-  RunConfig(App.gcn,       Dataset.papers100M,  pipeline=True),
-  RunConfig(App.gcn,       Dataset.friendster,  pipeline=True),
+      RunConfig(App.pinsage,   Dataset.reddit     ),
+      RunConfig(App.pinsage,   Dataset.products   ),
+      RunConfig(App.pinsage,   Dataset.papers100M ),
+      RunConfig(App.pinsage,   Dataset.friendster ),
 
-  RunConfig(App.graphsage, Dataset.reddit,      pipeline=True),
-  RunConfig(App.graphsage, Dataset.products,    pipeline=True),
-  RunConfig(App.graphsage, Dataset.papers100M,  pipeline=True),
-  RunConfig(App.graphsage, Dataset.friendster,  pipeline=True),
+      RunConfig(App.gcn,       Dataset.reddit,      pipeline=True),
+      RunConfig(App.gcn,       Dataset.products,    pipeline=True),
+      RunConfig(App.gcn,       Dataset.papers100M,  pipeline=True),
+      RunConfig(App.gcn,       Dataset.friendster,  pipeline=True),
 
-  RunConfig(App.pinsage,   Dataset.reddit,      pipeline=True),
-  RunConfig(App.pinsage,   Dataset.products,    pipeline=True),
-  RunConfig(App.pinsage,   Dataset.papers100M,  pipeline=True),
-  RunConfig(App.pinsage,   Dataset.friendster,  pipeline=True),
-]
+      RunConfig(App.graphsage, Dataset.reddit,      pipeline=True),
+      RunConfig(App.graphsage, Dataset.products,    pipeline=True),
+      RunConfig(App.graphsage, Dataset.papers100M,  pipeline=True),
+      RunConfig(App.graphsage, Dataset.friendster,  pipeline=True),
 
-conf_list += [
-  RunConfig(App.gcn,       Dataset.reddit,     cache_policy=CachePolicy.cache_by_degree,     cache_percent=0.98),
-  RunConfig(App.gcn,       Dataset.products,   cache_policy=CachePolicy.cache_by_degree,     cache_percent=0.98),
-  RunConfig(App.gcn,       Dataset.papers100M, cache_policy=CachePolicy.cache_by_heuristic,  cache_percent=0.25),
-  RunConfig(App.gcn,       Dataset.friendster, cache_policy=CachePolicy.cache_by_degree,     cache_percent=0.25),
+      RunConfig(App.pinsage,   Dataset.reddit,      pipeline=True),
+      RunConfig(App.pinsage,   Dataset.products,    pipeline=True),
+      RunConfig(App.pinsage,   Dataset.papers100M,  pipeline=True),
+      RunConfig(App.pinsage,   Dataset.friendster,  pipeline=True),
 
-  RunConfig(App.graphsage, Dataset.reddit,     cache_policy=CachePolicy.cache_by_degree,     cache_percent=0.98),
-  RunConfig(App.graphsage, Dataset.products,   cache_policy=CachePolicy.cache_by_degree,     cache_percent=0.98),
-  RunConfig(App.graphsage, Dataset.papers100M, cache_policy=CachePolicy.cache_by_heuristic,  cache_percent=0.25),
-  RunConfig(App.graphsage, Dataset.friendster, cache_policy=CachePolicy.cache_by_degree,     cache_percent=0.25),
+      RunConfig(App.gcn,       Dataset.reddit,     cache_policy=CachePolicy.cache_by_degree,     cache_percent=0.98),
+      RunConfig(App.gcn,       Dataset.products,   cache_policy=CachePolicy.cache_by_degree,     cache_percent=0.98),
+      RunConfig(App.gcn,       Dataset.papers100M, cache_policy=CachePolicy.cache_by_heuristic,  cache_percent=0.25),
+      RunConfig(App.gcn,       Dataset.friendster, cache_policy=CachePolicy.cache_by_degree,     cache_percent=0.25),
 
-  RunConfig(App.pinsage,   Dataset.reddit,     cache_policy=CachePolicy.cache_by_degree,     cache_percent=0.98),
-  RunConfig(App.pinsage,   Dataset.products,   cache_policy=CachePolicy.cache_by_degree,     cache_percent=0.98),
-  RunConfig(App.pinsage,   Dataset.papers100M, cache_policy=CachePolicy.cache_by_heuristic,  cache_percent=0.25),
-  RunConfig(App.pinsage,   Dataset.friendster, cache_policy=CachePolicy.cache_by_degree,     cache_percent=0.25),
-]
+      RunConfig(App.graphsage, Dataset.reddit,     cache_policy=CachePolicy.cache_by_degree,     cache_percent=0.98),
+      RunConfig(App.graphsage, Dataset.products,   cache_policy=CachePolicy.cache_by_degree,     cache_percent=0.98),
+      RunConfig(App.graphsage, Dataset.papers100M, cache_policy=CachePolicy.cache_by_heuristic,  cache_percent=0.25),
+      RunConfig(App.graphsage, Dataset.friendster, cache_policy=CachePolicy.cache_by_degree,     cache_percent=0.25),
 
-conf_list += [
-  RunConfig(App.gcn,       Dataset.reddit,     cache_policy=CachePolicy.cache_by_degree,    cache_percent=1.0,  pipeline=True, max_copying_jobs=2),
-  RunConfig(App.gcn,       Dataset.products,   cache_policy=CachePolicy.cache_by_degree,    cache_percent=1.0,  pipeline=True, max_copying_jobs=2),
-  RunConfig(App.gcn,       Dataset.papers100M, cache_policy=CachePolicy.cache_by_heuristic, cache_percent=0.25, pipeline=True), # 2
-  RunConfig(App.gcn,       Dataset.friendster, cache_policy=CachePolicy.cache_by_degree,    cache_percent=0.25, pipeline=True), # 2
+      RunConfig(App.pinsage,   Dataset.reddit,     cache_policy=CachePolicy.cache_by_degree,     cache_percent=0.98),
+      RunConfig(App.pinsage,   Dataset.products,   cache_policy=CachePolicy.cache_by_degree,     cache_percent=0.98),
+      RunConfig(App.pinsage,   Dataset.papers100M, cache_policy=CachePolicy.cache_by_heuristic,  cache_percent=0.25),
+      RunConfig(App.pinsage,   Dataset.friendster, cache_policy=CachePolicy.cache_by_degree,     cache_percent=0.25),
 
-  RunConfig(App.graphsage, Dataset.reddit,     cache_policy=CachePolicy.cache_by_degree,    cache_percent=1.0,  pipeline=True), # 2
-  RunConfig(App.graphsage, Dataset.products,   cache_policy=CachePolicy.cache_by_degree,    cache_percent=1.0,  pipeline=True), # 2
-  RunConfig(App.graphsage, Dataset.papers100M, cache_policy=CachePolicy.cache_by_heuristic, cache_percent=0.4,  pipeline=True), # 2
-  RunConfig(App.graphsage, Dataset.friendster, cache_policy=CachePolicy.cache_by_degree,    cache_percent=0.4,  pipeline=True, max_copying_jobs=2),
+      RunConfig(App.gcn,       Dataset.reddit,     cache_policy=CachePolicy.dynamic_cache),
+      RunConfig(App.gcn,       Dataset.products,   cache_policy=CachePolicy.dynamic_cache),
+      RunConfig(App.gcn,       Dataset.papers100M, cache_policy=CachePolicy.dynamic_cache),
+      RunConfig(App.gcn,       Dataset.friendster, cache_policy=CachePolicy.dynamic_cache),
 
-  RunConfig(App.pinsage,   Dataset.reddit,     cache_policy=CachePolicy.cache_by_degree,    cache_percent=1.0,  pipeline=True),
-  RunConfig(App.pinsage,   Dataset.products,   cache_policy=CachePolicy.cache_by_degree,    cache_percent=1.0,  pipeline=True),
-  RunConfig(App.pinsage,   Dataset.papers100M, cache_policy=CachePolicy.cache_by_heuristic, cache_percent=0.4,  pipeline=True),
-  RunConfig(App.pinsage,   Dataset.friendster, cache_policy=CachePolicy.cache_by_degree,    cache_percent=0.4,  pipeline=True),
-]
+      RunConfig(App.graphsage, Dataset.reddit,     cache_policy=CachePolicy.dynamic_cache),
+      RunConfig(App.graphsage, Dataset.products,   cache_policy=CachePolicy.dynamic_cache),
+      RunConfig(App.graphsage, Dataset.papers100M, cache_policy=CachePolicy.dynamic_cache),
+      RunConfig(App.graphsage, Dataset.friendster, cache_policy=CachePolicy.dynamic_cache),
 
-os.system('mkdir -p {}'.format(LOG_DIR))
-for cfg in conf_list:
-  cfg.run(False)
+      RunConfig(App.pinsage,   Dataset.reddit,     cache_policy=CachePolicy.dynamic_cache),
+      RunConfig(App.pinsage,   Dataset.products,   cache_policy=CachePolicy.dynamic_cache),
+      RunConfig(App.pinsage,   Dataset.papers100M, cache_policy=CachePolicy.dynamic_cache),
+      RunConfig(App.pinsage,   Dataset.friendster, cache_policy=CachePolicy.dynamic_cache),
+
+      RunConfig(App.gcn,       Dataset.reddit,     cache_policy=CachePolicy.cache_by_degree,    cache_percent=1.0,  pipeline=True, max_copying_jobs=2),
+      RunConfig(App.gcn,       Dataset.products,   cache_policy=CachePolicy.cache_by_degree,    cache_percent=1.0,  pipeline=True, max_copying_jobs=2),
+      RunConfig(App.gcn,       Dataset.papers100M, cache_policy=CachePolicy.cache_by_heuristic, cache_percent=0.25, pipeline=True), # 2
+      RunConfig(App.gcn,       Dataset.friendster, cache_policy=CachePolicy.cache_by_degree,    cache_percent=0.25, pipeline=True), # 2
+
+      RunConfig(App.graphsage, Dataset.reddit,     cache_policy=CachePolicy.cache_by_degree,    cache_percent=1.0,  pipeline=True), # 2
+      RunConfig(App.graphsage, Dataset.products,   cache_policy=CachePolicy.cache_by_degree,    cache_percent=1.0,  pipeline=True), # 2
+      RunConfig(App.graphsage, Dataset.papers100M, cache_policy=CachePolicy.cache_by_heuristic, cache_percent=0.4,  pipeline=True), # 2
+      RunConfig(App.graphsage, Dataset.friendster, cache_policy=CachePolicy.cache_by_degree,    cache_percent=0.4,  pipeline=True, max_copying_jobs=2),
+
+      RunConfig(App.pinsage,   Dataset.reddit,     cache_policy=CachePolicy.cache_by_degree,    cache_percent=1.0,  pipeline=True),
+      RunConfig(App.pinsage,   Dataset.products,   cache_policy=CachePolicy.cache_by_degree,    cache_percent=1.0,  pipeline=True),
+      RunConfig(App.pinsage,   Dataset.papers100M, cache_policy=CachePolicy.cache_by_heuristic, cache_percent=0.4,  pipeline=True),
+      RunConfig(App.pinsage,   Dataset.friendster, cache_policy=CachePolicy.cache_by_degree,    cache_percent=0.4,  pipeline=True),
+
+      RunConfig(App.gcn,       Dataset.reddit,     cache_policy=CachePolicy.dynamic_cache, pipeline=True),
+      RunConfig(App.gcn,       Dataset.products,   cache_policy=CachePolicy.dynamic_cache, pipeline=True),
+      RunConfig(App.gcn,       Dataset.papers100M, cache_policy=CachePolicy.dynamic_cache, pipeline=True),
+      RunConfig(App.gcn,       Dataset.friendster, cache_policy=CachePolicy.dynamic_cache, pipeline=True),
+
+      RunConfig(App.graphsage, Dataset.reddit,     cache_policy=CachePolicy.dynamic_cache, pipeline=True),
+      RunConfig(App.graphsage, Dataset.products,   cache_policy=CachePolicy.dynamic_cache, pipeline=True),
+      RunConfig(App.graphsage, Dataset.papers100M, cache_policy=CachePolicy.dynamic_cache, pipeline=True),
+      RunConfig(App.graphsage, Dataset.friendster, cache_policy=CachePolicy.dynamic_cache, pipeline=True),
+
+      RunConfig(App.pinsage,   Dataset.reddit,     cache_policy=CachePolicy.dynamic_cache, pipeline=True),
+      RunConfig(App.pinsage,   Dataset.products,   cache_policy=CachePolicy.dynamic_cache, pipeline=True),
+      RunConfig(App.pinsage,   Dataset.papers100M, cache_policy=CachePolicy.dynamic_cache, pipeline=True),
+      RunConfig(App.pinsage,   Dataset.friendster, cache_policy=CachePolicy.dynamic_cache, pipeline=True),
+    ]
+  
+  def select_app(self, app_indicator):
+    newlist = []
+    for cfg in self.conf_list:
+      if cfg.app in app_indicator:
+        newlist.append(cfg)
+    self.conf_list = newlist
+    return self
+  def select_dataset(self, ds_indicator):
+    newlist = []
+    for cfg in self.conf_list:
+      if cfg.dataset in ds_indicator:
+        newlist.append(cfg)
+    self.conf_list = newlist
+    return self
+  def select_cache(self, cache_indicator):
+    newlist = []
+    for cfg in self.conf_list:
+      if cfg.cache_policy in cache_indicator:
+        newlist.append(cfg)
+    self.conf_list = newlist
+    return self
+  def select_pipe(self, pipe_indicator):
+    newlist = []
+    for cfg in self.conf_list:
+      if cfg.pipeline in pipe_indicator:
+        newlist.append(cfg)
+    self.conf_list = newlist
+    return self
+
+run_in_list(ConfigList()
+  .select_app([
+    App.gcn,
+    # App.graphsage,
+    # App.pinsage,
+  ]).select_dataset([
+    Dataset.reddit,
+    # Dataset.products,
+    # Dataset.papers100M,
+    # Dataset.friendster,
+  ]).select_cache([
+    # CachePolicy.no_cache,
+    # CachePolicy.cache_by_degree,
+    # CachePolicy.cache_by_heuristic,
+    CachePolicy.dynamic_cache,
+  ]).select_pipe([
+    # False,
+    True,
+  ]).conf_list)
 
