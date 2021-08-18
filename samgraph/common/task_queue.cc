2bc86d30274904f50910bc6b67cf7743e546a828
Tang Dahai <seatang@hnu.edu.cn>
2021-08-18 23:13:29 +0800

    Serialization in Task
    Is there a easier method to solve Task serialization?

--- samgraph/common/task_queue.cc
+++ samgraph/common/task_queue.cc
@@ -1,4 +1,5 @@
 #include "task_queue.h"
+#include "memory_queue.h"
 
 namespace samgraph {
 namespace common {
@@ -30,6 +31,86 @@ std::shared_ptr<Task> TaskQueue::GetTask() {
     return task;
   }
   return nullptr;
+}
+
+namespace {
+  struct TransData {
+    bool     weight; // if have weight values
+    int      num_layer;
+    uint64_t key;
+    size_t   input_size;
+    size_t   output_size;
+    uint32_t data[0];
+    // input_nodes|output_nodes|
+    //   GraphData: <num_src|num_dst|num_edge|row|col|data> : layer 1
+    //   GraphData: <num_src|num_dst|num_edge|row|col|data> : layer 2
+    //   ...
+  };
+  struct GraphData {
+    size_t num_src;
+    size_t num_dst;
+    size_t num_edge;
+    uint32_t data[0];
+    // row|col|[data]
+  };
+  size_t GetSize(std::shared_ptr<Task> task) {
+    size_t ret = 0;
+    ret += task->input_nodes->NumBytes();
+    ret += task->output_nodes->NumBytes();
+    for (auto &graph : task->graphs) {
+      ret += 3 * sizeof(size_t);
+      ret += graph->row->NumBytes();
+      ret += graph->col->NumBytes();
+      if (graph->data != nullptr) {
+        ret += graph->data->NumBytes();
+      }
+    }
+    return ret;
+  }
+  void* ToData(std::shared_ptr<Task> task) {
+    bool weight = false;
+    if (task->graphs[0]->data != nullptr) {
+      weight = true;
+    }
+    size_t data_size = GetSize(task);
+    TransData* ptr = malloc(sizeof(TransData) + data_size);
+    ptr->weight = weight;
+    ptr->num_layer = (task->graphs.size());
+    ptr->key = task->key;
+    ptr->input_size = task->input_nodes->Shape()[0];
+    ptr->output_size = task->output_nodes->Shape()[0];
+    std::memcpy(ptr->data, task->input_nodes->Data(),
+                sizeof(uint32_t) * ptr->input_size);
+    std::memcpy(ptr->data + ptr->input_size, task->output_nodes->Data(),
+                sizeof(uint32_t) * ptr->output_size);
+    GraphData* graph_data = ptr->data + ptr->input_size + ptr->output_size;
+    for (auto &graph : task->graphs) {
+      graph_data->num_src = graph->num_src;
+      graph_data->num_dst = graph->num_dst;
+      graph_data->num_edge = graph->num_edge;
+      std::memcpy(graph_data->data, graph->row->Data(),
+                  graph->row->NumBytes());
+      std::memcpy(graph_data->data + graph->num_edge,
+                  graph->col->Data(),
+                  graph->col->NumBytes());
+      if (weight) {
+        std::memcpy(graph_data->data + 2 * graph->num_edge,
+                    graph->data->Data(),
+                    graph->data->NumBytes());
+        graph_data = (graph_data + 3 * graph->num_edge);
+      } else {
+        graph_data = (graph_data + 2 * graph->num_edge);
+      }
+    }
+    return static_cast<void*>(ptr);
+  }
+  std::shared_ptr<Task> ParseData(void* ptr) {
+
+  }
+} // namespace
+
+bool TaskQueue::Send(std::shared_ptr<Task> task) {
+
 }
 
 }  // namespace common
