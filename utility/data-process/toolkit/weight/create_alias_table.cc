0f085131eb7dfb2148a7d82765e9b8d8c789aeaa
sxn <xiaoniu.sxn@outlook.com>
2021-09-14 22:19:26 +0800

    allow custom weight

--- utility/data-process/toolkit/weight/create_alias_table.cc
+++ utility/data-process/toolkit/weight/create_alias_table.cc
@@ -4,6 +4,7 @@
 #include <random>
 #include <string>
 #include <vector>
+#include <unordered_map>
 
 #include "common/graph_loader.h"
 #include "common/options.h"
@@ -13,9 +14,34 @@ namespace {
 
 std::string output0_filepath = "prob_table.bin";
 std::string output1_filepath = "alias_table.bin";
+std::shared_ptr<utility::DegreeInfo> degree_info;
+
+#define WEIGHT_POLICY_TYPES( F ) \
+  F(kDefault) \
+  F(kInverseBothDegreeRand) \
+  F(kInverseSrcDegreeRand)
+
+#define F(name) name,
+enum WeightPolicy {WEIGHT_POLICY_TYPES( F ) kNumItems };
+#undef F
+WeightPolicy weight_policy = kDefault;
+
+#define F(name) {#name,name},
+std::unordered_map<std::string, WeightPolicy> policy_str_to_int = {
+WEIGHT_POLICY_TYPES( F )
+};
+#undef F
+
+void InitPolicy(std::string policy_str) {
+  if (policy_str_to_int.find(policy_str) == policy_str_to_int.end()) {
+    utility::Check(false, "wrong policy " + policy_str);
+  }
+  weight_policy = policy_str_to_int[policy_str];
+}
 
 uint32_t RandomInt(const uint32_t &min, const uint32_t &max) {
-  static thread_local std::mt19937 generator;
+  static thread_local std::random_device dev;
+  static thread_local std::mt19937 generator(dev());
   std::uniform_int_distribution<uint32_t> distribution(min, max);
   return distribution(generator);
 }
@@ -27,6 +53,17 @@ void AddPrefixToFilepath(std::string prefix) {
 
   output0_filepath = prefix + output0_filepath;
   output1_filepath = prefix + output1_filepath;
+}
+
+float InverseSrcDegreeRand(uint32_t src, uint32_t dst) {
+  uint32_t src_out_deg = degree_info->out_degrees[src];
+  // return 1.0 / RandomInt(1, src_out_deg);
+  return 1.0 / src_out_deg;
+}
+float InverseBothDegreeRand(uint32_t src, uint32_t dst) {
+  uint32_t src_out_deg = degree_info->out_degrees[src];
+  uint32_t dst_in_deg = degree_info->in_degrees[dst];
+  return 1.0 / RandomInt(1, std::max(src_out_deg, dst_in_deg));
 }
 
 void CreateAliasTable(const uint32_t *indptr, const uint32_t *indices,
@@ -43,7 +80,17 @@ void CreateAliasTable(const uint32_t *indptr, const uint32_t *indices,
     float weight_sum = 0.0f;
 
     for (uint32_t i = 0; i < len; i++) {
-      weights[i] = static_cast<float>(RandomInt(1, 10));
+      uint32_t dst = nodeid, src = indices[off+i];
+      switch(weight_policy) {
+        case kDefault:
+          weights[i] = static_cast<float>(RandomInt(1, 10)); break;
+        case kInverseBothDegreeRand:
+          weights[i] = InverseBothDegreeRand(src, dst); break;
+        case kInverseSrcDegreeRand:
+          weights[i] = InverseSrcDegreeRand(src, dst); break;
+        default:
+          utility::Check(false);
+      }
       weight_sum += weights[i];
     }
 
@@ -116,11 +163,15 @@ void CreateAliasTable(const uint32_t *indptr, const uint32_t *indices,
 }  // namespace
 
 int main(int argc, char *argv[]) {
+  std::string policy_str;
+  utility::Options::CustomOption("-P,--policy", policy_str);
   utility::Options::InitOptions("Graph property");
   OPTIONS_PARSE(argc, argv);
+  InitPolicy(policy_str);
 
   utility::GraphLoader graph_loader(utility::Options::root);
   auto graph = graph_loader.GetGraphDataset(utility::Options::graph);
+  degree_info = utility::DegreeInfo::GetDegrees(graph);
 
   uint32_t *indptr = graph->indptr;
   uint32_t *indices = graph->indices;
