259a83c0d0616a00c5ad0c537f827da59a4fe956
Dahai Tang <seatang@hnu.edu.cn>
2021-09-21 21:11:08 +0800

    Feat: parse result in naive running script

--- example/samgraph/run_multi.py
+++ example/samgraph/run_multi.py
@@ -3,66 +3,98 @@ import sys
 import time
 import re
 
-dataset_root = '/graph-learning/samgraph/'
 log_dir = '/home/tangdahai.tdh/dahai-logs/{}/'.format(time.strftime("%Y-%m-%d_%H-%M-%S"))
+file_dir = log_dir + 'log/'
+out_dir = log_dir + 'out/'
 
-dataset_list = ['papers100M']
-# dataset_list = ['reddit']
-omp_thread_list = [4,8,12,16,24,32,40,48,64,72,86,96]
-# omp_thread_list = [4]
-num_sampler = [1]
-num_trainer = [1]
-cache_policy = [1]
-max_copying_jobs = [1]
-mock = False
+run_config_list = {}
+run_config_list[' export SAMGRAPH_OMP_NUM_THREADS={}; '] = [12, 20, 24, 28, 32, 40]
+run_config_list[' python ./{}'] = ['train_gcn_multi_gpu.py']
+run_config_list[' --dataset-path /graph-learning/samgraph/{}'] = ['papers100M']
+run_config_list[' --num-sample-worker {}'] =         [1]
+run_config_list[' --num-train-worker {}'] =          [1, 2, 3, 4]
+run_config_list[' --cache-policy {}'] =              [1]
+run_config_list[' --max-copying-jobs {}'] =          [1]
+run_config_list[' --num-epoch {}'] =                 [3]
+run_config_list[' --cache-percentage {}'] =          [0.22]
+run_config_list[' --pipeline '] =                    [1]
+mock = True
 
 def run_app():
-    if (not os.path.exists(log_dir)):
-        cmd = f'mkdir -p {log_dir}'
+    if (not os.path.exists(file_dir)):
+        cmd = f'mkdir -p {file_dir}; mkdir -p {out_dir}'
         if (mock):
             print(cmd)
         else:
             os.system(cmd)
-    for dataset in dataset_list:
-        log_file_prefix = log_dir + dataset
-        dataset = dataset_root + dataset
-        for omp_num_thread in omp_thread_list:
-            log_file = log_file_prefix + f'_{omp_num_thread}'
-            cmd = f'export SAMGRAPH_OMP_NUM_THREADS={omp_num_thread}; '
-            cmd += f'python train_gcn_multi_gpu.py --dataset {dataset}'
-            cmd += f' --num-epoch 3 --cache-percentage 0.22 --num-sample-worker 1 --num-train-worker 1 --cache-policy 1 --max-copying-jobs 1'
-            cmd += f' --log-file {log_file}'
-            if (mock):
-                print(cmd)
-            else:
-                os.system(cmd)
+    total_comb = int(1)
+    for k,l in run_config_list.items():
+        total_comb *= len(l)
+    print('total_comb: ', total_comb)
+    config_idx_file = log_dir + f'config_list.txt'
+    for comb_idx in range(total_comb):
+        file_name = file_dir + f'/{comb_idx}.log'
+        out_name = out_dir + f'/{comb_idx}.out.log'
+        old_comb_idx = comb_idx
+        cmd_str = ''
+        config_str = ''
+        for k,l in run_config_list.items():
+            len_l = len(l)
+            mod = comb_idx % len_l
+            comb_idx = comb_idx // len_l
+            cmd_str += k.format(l[mod])
+            config_str += k.format(l[mod]) + '\n'
+        cmd_str += f' --log-file {file_name}'
+        cmd_str += f' &> {out_name}'
+        if (mock):
+            print(cmd_str)
+        else:
+            with open(config_idx_file, 'a+', encoding='utf8') as config_file:
+                print(f'\n {old_comb_idx}: ', file=config_file)
+                print(config_str, file=config_file)
+            os.system(cmd_str)
 
-def run_parse_log(log_dir, pick_map):
+def run_parse_log(file_dir, pick_map):
+    import numpy as np
+    for (k,v) in pick_map.items():
+        print(k, '\t', end="")
+    print('')
+
     config_pattern = r'config:(.+)=(.+)\n'
     result_pattern = r'test_result:(.+)=(.+)\n'
-    files = os.listdir(log_dir)
+    files = os.listdir(file_dir)
     for file_name in files:
         if (os.path.isdir(file_name)):
             continue
-        with open(log_dir + "/" + file_name, 'r', encoding='utf8') as log_file:
+        with open(file_dir + "/" + file_name, 'r', encoding='utf8') as log_file:
             for line in log_file:
                 config = re.match(config_pattern, line)
                 result = re.match(result_pattern, line)
                 if config:
                     key = config.group(1)
                     value = config.group(2)
+                    if (key in pick_map.keys()):
+                        pick_map[key] = value
                 if result:
                     key = result.group(1)
                     value = result.group(2)
-                if (key in pick_map.keys()):
-                    pick_map[key] = value
+                    if (key in pick_map.keys()):
+                        pick_map[key].append(float(value))
             for (k,v) in pick_map.items():
-                print(v, '\t', end="")
+                res = v
+                if isinstance(v, list):
+                    res = '{:.2f}'.format(np.mean(v[0:]))
+                print(res, '\t', end="")
+                if isinstance(v, list):
+                    v.clear()
             print('')
 
 
 if __name__ == '__main__':
     # run_app()
-    pick_map = { "omp_num_threads" : "", "sample_time" : "", "copy_time" : "",
-            "convert_time" : "", "train_time" : "", "epoch_time" : ""}
-    run_parse_log('/home/tangdahai.tdh/dahai-logs/2021-09-20_22-43-46/', pick_map)
+    pick_map = { "omp_num_threads" : "", "num_sample_worker" : "", "num_train_worker" : "",
+        "sample_time" : [], "copy_time" : [],
+        "convert_time" : [], "train_time" : [], "epoch_time" : []}
+    run_parse_log('/home/tangdahai.tdh/dahai-logs/2021-09-21_19-22-46/log/', pick_map)
+    '''
+    '''
