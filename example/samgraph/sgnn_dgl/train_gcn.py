75ac40617f050d8ab21eaa7a7913bf1b65150d71
Jianbang Yang <yangjianbang112@gmail.com>
2021-10-04 18:56:06 -0700

    fix sgnn dgl

--- example/samgraph/sgnn_dgl/train_gcn.py
+++ example/samgraph/sgnn_dgl/train_gcn.py
@@ -92,7 +92,7 @@ def get_run_config():
 
 
 def run(worker_id, run_config):
-    torch.set_num_threads(run_config['torch_num_thread'])
+    torch.set_num_threads(run_config['torch_thread_num'])
     num_worker = run_config['num_worker']
     global_barrier = run_config['global_barrier']
     ctx = run_config['workers'][worker_id]
