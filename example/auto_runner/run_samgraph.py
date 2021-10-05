4f64519a122623b420b05e86f8c24c6f1f73e323
Jianbang Yang <yangjianbang112@gmail.com>
2021-10-05 08:09:17 -0700

    add script

--- example/auto_runner/run_samgraph.py
+++ example/auto_runner/run_samgraph.py
@@ -247,96 +247,85 @@ def breakdown_test(log_folder=None):
     print('breakdown test uses {:.4f} secs'.format(toc - tic))
 
 
-def overall_test(log_folder):
+def overall_test(log_folder=None, mock=False):
     tic = time.time()
 
     if log_folder:
-        mock = True
         log_dir = os.path.join(os.path.join(here, f'run-logs/{log_folder}'))
     else:
-        mock = False
         log_dir = os.path.join(
             here, f'run-logs/logs_samgraph_{datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}')
 
-    # log_table = LogTable(
-    #     num_row=12,
-    #     num_col=5
-    # ).update_col_definition(
-    #     col_id=0,
-    #     definition='sample_time'
-    # ).update_col_definition(
-    #     col_id=1,
-    #     definition='copy_time'
-    # ).update_col_definition(
-    #     col_id=2,
-    #     definition='convert_time'
-    # ).update_col_definition(
-    #     col_id=3,
-    #     definition='train_time'
-    # ).update_col_definition(
-    #     col_id=4,
-    #     definition='epoch_time'
-    # ).update_row_definition(
-    #     row_id=0,
-    #     col_range=[0, 9],
-    #     app=App.gcn,
-    #     dataset=Dataset.products
-    # ).update_row_definition(
-    #     row_id=1,
-    #     col_range=[0, 9],
-    #     app=App.gcn,
-    #     dataset=Dataset.papers100M
-    # ).update_row_definition(
-    #     row_id=2,
-    #     col_range=[0, 9],
-    #     app=App.gcn,
-    #     dataset=Dataset.twitter
-    # ).update_row_definition(
-    #     row_id=3,
-    #     col_range=[0, 9],
-    #     app=App.gcn,
-    #     dataset=Dataset.uk_2006_05
-    # ).update_row_definition(
-    #     row_id=4,
-    #     col_range=[0, 9],
-    #     app=App.graphsage,
-    #     dataset=Dataset.products
-    # ).update_row_definition(
-    #     row_id=5,
-    #     col_range=[0, 9],
-    #     app=App.graphsage,
-    #     dataset=Dataset.papers100M
-    # ).update_row_definition(
-    #     row_id=6,
-    #     col_range=[0, 9],
-    #     app=App.graphsage,
-    #     dataset=Dataset.twitter
-    # ).update_row_definition(
-    #     row_id=7,
-    #     col_range=[0, 9],
-    #     app=App.graphsage,
-    #     dataset=Dataset.uk_2006_05
-    # ).update_row_definition(
-    #     row_id=8,
-    #     col_range=[0, 9],
-    #     app=App.pinsage,
-    #     dataset=Dataset.products
-    # ).update_row_definition(
-    #     row_id=9,
-    #     col_range=[0, 9],
-    #     app=App.pinsage,
-    #     dataset=Dataset.papers100M
-    # ).update_row_definition(
-    #     row_id=10,
-    #     col_range=[0, 9],
-    #     app=App.pinsage,
-    #     dataset=Dataset.twitter
-    # ).update_row_definition(
-    #     row_id=11,
-    #     col_range=[0, 9],
-    #     app=App.pinsage,
-    #     dataset=Dataset.uk_2006_05
-    # ).create()
+    log_table = LogTable(
+        num_row=12,
+        num_col=1
+    ).update_col_definition(
+        col_id=0,
+        definition='pipeline_train_epoch_time'
+    ).update_row_definition(
+        row_id=0,
+        col_range=[0, 0],
+        app=App.gcn,
+        dataset=Dataset.products
+    ).update_row_definition(
+        row_id=1,
+        col_range=[0, 0],
+        app=App.gcn,
+        dataset=Dataset.papers100M
+    ).update_row_definition(
+        row_id=2,
+        col_range=[0, 0],
+        app=App.gcn,
+        dataset=Dataset.twitter
+    ).update_row_definition(
+        row_id=3,
+        col_range=[0, 0],
+        app=App.gcn,
+        dataset=Dataset.uk_2006_05
+    ).update_row_definition(
+        row_id=4,
+        col_range=[0, 0],
+        app=App.graphsage,
+        dataset=Dataset.products
+    ).update_row_definition(
+        row_id=5,
+        col_range=[0, 0],
+        app=App.graphsage,
+        dataset=Dataset.papers100M,
+        num_sample_worker=2
+    ).update_row_definition(
+        row_id=6,
+        col_range=[0, 0],
+        app=App.graphsage,
+        dataset=Dataset.twitter,
+        num_sample_worker=2
+    ).update_row_definition(
+        row_id=7,
+        col_range=[0, 0],
+        app=App.graphsage,
+        dataset=Dataset.uk_2006_05,
+        num_sample_worker=1
+    ).update_row_definition(
+        row_id=8,
+        col_range=[0, 0],
+        app=App.pinsage,
+        dataset=Dataset.products
+    ).update_row_definition(
+        row_id=9,
+        col_range=[0, 0],
+        app=App.pinsage,
+        dataset=Dataset.papers100M
+    ).update_row_definition(
+        row_id=10,
+        col_range=[0, 0],
+        app=App.pinsage,
+        dataset=Dataset.twitter
+    ).update_row_definition(
+        row_id=11,
+        col_range=[0, 0],
+        app=App.pinsage,
+        dataset=Dataset.uk_2006_05
+    ).create()
 
     ConfigList(
         test_group_name='Samgraph overall test'
@@ -379,31 +368,40 @@ def overall_test(log_folder):
     ).multi_combo_multi_override(
         'and',
         {'app': [App.gcn], 'dataset': [Dataset.papers100M]},
-        {'cache_percentage': 0.18, 'num_sample_worker': 2,  'num_train_worker': 6}
+        {'cache_percentage': 0.19, 'num_sample_worker': 2,  'num_train_worker': 6}
     ).multi_combo_multi_override(
         'and',
         {'app': [App.gcn], 'dataset': [Dataset.twitter]},
-        {'cache_percentage': 0.20, 'num_sample_worker': 2,  'num_train_worker': 6}
+        {'cache_percentage': 0.22, 'num_sample_worker': 2,  'num_train_worker': 6}
     ).multi_combo_multi_override(
         'and',
         {'app': [App.gcn], 'dataset': [Dataset.uk_2006_05]},
-        {'cache_percentage': 0.11, 'num_sample_worker': 2,  'num_train_worker': 6}
+        {'cache_percentage': 0.08, 'num_sample_worker': 2,  'num_train_worker': 6}
     ).multi_combo_multi_override(
         'and',
         {'app': [App.graphsage], 'dataset': [Dataset.products]},
         {'cache_percentage': 1.0, 'num_sample_worker': 4,  'num_train_worker': 4}
-    ).multi_combo_multi_override(
+    ).multi_combo_multi_override_list(
         'and',
         {'app': [App.graphsage], 'dataset': [Dataset.papers100M]},
-        {'cache_percentage': 0.22, 'num_sample_worker': 2,  'num_train_worker': 6}
-    ).multi_combo_multi_override(
+        [
+            {'cache_percentage': 0.23, 'num_sample_worker': 2,  'num_train_worker': 6},
+            {'cache_percentage': 0.23, 'num_sample_worker': 3,  'num_train_worker': 5},
+        ]
+    ).multi_combo_multi_override_list(
         'and',
         {'app': [App.graphsage], 'dataset': [Dataset.twitter]},
-        {'cache_percentage': 0.30, 'num_sample_worker': 2,  'num_train_worker': 6}
-    ).multi_combo_multi_override(
+        [
+            {'cache_percentage': 0.30, 'num_sample_worker': 2,  'num_train_worker': 6},
+            {'cache_percentage': 0.30, 'num_sample_worker': 3,  'num_train_worker': 5}
+        ]
+    ).multi_combo_multi_override_list(
         'and',
         {'app': [App.graphsage], 'dataset': [Dataset.uk_2006_05]},
-        {'cache_percentage': 0.15, 'num_sample_worker': 1,  'num_train_worker': 7}
+        [
+            {'cache_percentage': 0.15, 'num_sample_worker': 1,  'num_train_worker': 7},
+            {'cache_percentage': 0.15, 'num_sample_worker': 2,  'num_train_worker': 6}
+        ]
     ).multi_combo_multi_override(
         'and',
         {'app': [App.pinsage], 'dataset': [Dataset.products]},
@@ -415,7 +413,7 @@ def overall_test(log_folder):
     ).multi_combo_multi_override(
         'and',
         {'app': [App.pinsage], 'dataset': [Dataset.twitter]},
-        {'cache_percentage': 0.23, 'num_sample_worker': 1,  'num_train_worker': 7}
+        {'cache_percentage': 0.21, 'num_sample_worker': 1,  'num_train_worker': 7}
     ).multi_combo_multi_override(
         'and',
         {'app': [App.pinsage], 'dataset': [Dataset.uk_2006_05]},
@@ -426,10 +424,10 @@ def overall_test(log_folder):
     ).run(
         appdir=app_dir,
         logdir=log_dir,
-        mock=False
-        # ).parse_logs(
-        #     logtable=log_table,
-        #     logdir=log_dir
+        mock=mock
+    ).parse_logs(
+        logtable=log_table,
+        logdir=log_dir
     )
 
     toc = time.time()
@@ -440,7 +438,8 @@ def overall_test(log_folder):
 if __name__ == '__main__':
     argparser = argparse.ArgumentParser("DGL runner")
     argparser.add_argument('-l', '--log-folder', default=None)
+    argparser.add_argument('-m', '--mock', action='store_true', default=False)
     args = argparser.parse_args()
 
-    breakdown_test(args.log_folder)
-    # overall_test(args.log_folder)
+    # breakdown_test(args.log_folder)
+    overall_test(args.log_folder, args.mock)
