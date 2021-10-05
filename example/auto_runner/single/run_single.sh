2246e521ed8aeda89d168405a54e1d88bc963823
Dahai Tang <seatang@hnu.edu.cn>
2021-10-06 00:08:24 +0800

    Fix: update the cache Pct. for single-gpu test

--- example/auto_runner/single/run_single.sh
+++ example/auto_runner/single/run_single.sh
@@ -21,13 +21,12 @@ mkdir -p $log_dir
 # multi_gpu for single gpu run
 python ${sam_dir}/train_gcn.py --dataset products --single-gpu --num-epoch ${num_epoch} --cache-percentage 1.0 2>&1 | tee ${log_dir}/sam_gcn_products_single.log
 python ${sam_dir}/train_gcn.py --dataset papers100M --single-gpu --num-epoch ${num_epoch} --cache-percentage 0.03 2>&1 | tee ${log_dir}/sam_gcn_papers_single.log
-python ${sam_dir}/train_gcn.py --dataset twitter --single-gpu --num-epoch ${num_epoch} --cache-percentage 0.04 2>&1 | tee ${log_dir}/sam_gcn_twitter_single.log
+python ${sam_dir}/train_gcn.py --dataset twitter --single-gpu --num-epoch ${num_epoch} --cache-percentage 0.0 2>&1 | tee ${log_dir}/sam_gcn_twitter_single.log
 
 python ${sam_dir}/train_graphsage.py --dataset products --single-gpu --num-epoch ${num_epoch} --cache-percentage 1.0 2>&1 | tee ${log_dir}/sam_graphsage_products_single.log
 python ${sam_dir}/train_graphsage.py --dataset papers100M --single-gpu --num-epoch ${num_epoch} --cache-percentage 0.07 2>&1 | tee ${log_dir}/sam_graphsage_papers_single.log
 python ${sam_dir}/train_graphsage.py --dataset twitter --single-gpu --num-epoch ${num_epoch} --cache-percentage 0.13 2>&1 | tee ${log_dir}/sam_graphsage_twitter_single.log
-python ${sam_dir}/train_graphsage.py --dataset uk-2006-05 --single-gpu --num-epoch ${num_epoch} --cache-percentage 0.0 2>&1 | tee ${log_dir}/sam_graphsage_uk_single.log
 
 python ${sam_dir}/train_pinsage.py --dataset products --single-gpu --num-epoch ${num_epoch} --cache-percentage 1.0 2>&1 | tee ${log_dir}/sam_pinsage_products_single.log
-python ${sam_dir}/train_pinsage.py --dataset papers100M --single-gpu --num-epoch ${num_epoch} --cache-percentage 0.05 2>&1 | tee ${log_dir}/sam_pinsage_papers_single.log
-python ${sam_dir}/train_pinsage.py --dataset twitter --single-gpu --num-epoch ${num_epoch} --cache-percentage 0.06 2>&1 | tee ${log_dir}/sam_pinsage_twitter_single.log
+python ${sam_dir}/train_pinsage.py --dataset papers100M --single-gpu --num-epoch ${num_epoch} --cache-percentage 0.03 2>&1 | tee ${log_dir}/sam_pinsage_papers_single.log
+python ${sam_dir}/train_pinsage.py --dataset twitter --single-gpu --num-epoch ${num_epoch} --cache-percentage 0.03 2>&1 | tee ${log_dir}/sam_pinsage_twitter_single.log
