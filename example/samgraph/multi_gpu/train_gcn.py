9c1e48e31307c8156ecf93e76ed5078519e397a3
sxn <xiaoniu.sxn@outlook.com>
2021-10-10 22:22:11 +0800

    fix indent

--- example/samgraph/multi_gpu/train_gcn.py
+++ example/samgraph/multi_gpu/train_gcn.py
@@ -430,8 +430,8 @@ if __name__ == '__main__':
 
     ret = sam.wait_one_child()
     if ret != 0:
-      for p in workers:
-        p.kill()
+        for p in workers:
+            p.kill()
     for p in workers:
         p.join()
 
