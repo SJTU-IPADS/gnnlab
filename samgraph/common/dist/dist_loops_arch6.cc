b6fcb241bc9d1547beeebdeec812ff95a02b075c
sxn <xiaoniu.sxn@outlook.com>
2021-10-10 22:29:56 +0800

    add 'start' to arch6

--- samgraph/common/dist/dist_loops_arch6.cc
+++ samgraph/common/dist/dist_loops_arch6.cc
@@ -34,9 +34,11 @@ namespace samgraph {
 namespace common {
 namespace dist {
 
-bool RunSampleCopySubLoopOnce() {
-  auto graph_pool = DistEngine::Get()->GetGraphPool();
-  if (graph_pool->Full()) {
+namespace {
+bool RunSampleSubLoopOnce() {
+  auto next_op = cuda::kDataCopy;
+  auto next_q = DistEngine::Get()->GetTaskQueue(next_op);
+  if (next_q->Full()) {
     std::this_thread::sleep_for(std::chrono::nanoseconds(1000));
     return true;
   }
@@ -50,6 +52,32 @@ bool RunSampleCopySubLoopOnce() {
     DoGPUSample(task);
     double sample_time = t1.Passed();
 
+    next_q->AddTask(task);
+
+    Profiler::Get().LogStep(task->key, kLogL1SampleTime,
+                            shuffle_time + sample_time);
+    Profiler::Get().LogStep(task->key, kLogL2ShuffleTime, shuffle_time);
+    Profiler::Get().LogEpochAdd(task->key, kLogEpochSampleTime,
+                                shuffle_time + sample_time);
+  } else {
+    std::this_thread::sleep_for(std::chrono::nanoseconds(1000));
+  }
+
+  return true;
+}
+
+bool RunDataCopySubLoopOnce() {
+  auto graph_pool = DistEngine::Get()->GetGraphPool();
+  if (graph_pool->Full()) {
+    std::this_thread::sleep_for(std::chrono::nanoseconds(1000));
+    return true;
+  }
+
+  auto this_op = cuda::kDataCopy;
+  auto this_q = DistEngine::Get()->GetTaskQueue(this_op);
+  auto task = this_q->GetTask();
+
+  if (task) {
     Timer t2;
     DoIdCopy(task);
     double id_copy_time = t2.Passed();
@@ -62,14 +90,9 @@ bool RunSampleCopySubLoopOnce() {
     DoFeatureCopy(task);
     double feat_copy_time = t4.Passed();
 
-    LOG(DEBUG) << "Submit with cache: process task with key " << task->key;
+    LOG(DEBUG) << "Submit: process task with key " << task->key;
     graph_pool->Submit(task->key, task);
 
-    Profiler::Get().LogStep(task->key, kLogL1SampleTime,
-                            shuffle_time + sample_time);
-    Profiler::Get().LogStep(task->key, kLogL2ShuffleTime, shuffle_time);
-    Profiler::Get().LogEpochAdd(task->key, kLogEpochSampleTime,
-                                shuffle_time + sample_time);
     Profiler::Get().LogStep(task->key, kLogL1CopyTime,
                             id_copy_time + extract_time + feat_copy_time);
     Profiler::Get().LogStep(task->key, kLogL2IdCopyTime, id_copy_time);
@@ -84,22 +107,18 @@ bool RunSampleCopySubLoopOnce() {
   return true;
 }
 
-bool RunCacheSampleCopySubLoopOnce() {
+bool RunCacheDataCopySubLoopOnce() {
   auto graph_pool = DistEngine::Get()->GetGraphPool();
   if (graph_pool->Full()) {
     std::this_thread::sleep_for(std::chrono::nanoseconds(1000));
     return true;
   }
 
-  Timer t0;
-  auto task = DoShuffle();
+  auto this_op = cuda::kDataCopy;
+  auto this_q = DistEngine::Get()->GetTaskQueue(this_op);
+  auto task = this_q->GetTask();
+
   if (task) {
-    double shuffle_time = t0.Passed();
-
-    Timer t1;
-    DoGPUSample(task);
-    double sample_time = t1.Passed();
-
     Timer t2;
     DoCacheIdCopyToCPU(task);
     double id_copy_time = t2.Passed();
@@ -112,13 +131,9 @@ bool RunCacheSampleCopySubLoopOnce() {
     LOG(DEBUG) << "Submit with cache: process task with key " << task->key;
     graph_pool->Submit(task->key, task);
 
-    Profiler::Get().LogStep(task->key, kLogL1SampleTime,
-                            shuffle_time + sample_time);
-    Profiler::Get().LogStep(task->key, kLogL2ShuffleTime, shuffle_time);
-    Profiler::Get().LogEpochAdd(task->key, kLogEpochSampleTime,
-                                shuffle_time + sample_time);
-    Profiler::Get().LogStep(task->key, kLogL1CopyTime,
-                            id_copy_time + feat_copy_time);
+    Profiler::Get().LogStep(
+        task->key, kLogL1CopyTime,
+        id_copy_time + feat_copy_time);
     Profiler::Get().LogStep(task->key, kLogL2IdCopyTime, id_copy_time);
     Profiler::Get().LogStep(task->key, kLogL2FeatCopyTime, feat_copy_time);
     Profiler::Get().LogEpochAdd(task->key, kLogEpochCopyTime,
@@ -130,12 +145,59 @@ bool RunCacheSampleCopySubLoopOnce() {
   return true;
 }
 
-void RunArch6LoopsOnce() {
-  if (!RunConfig::UseGPUCache()) {
-    RunSampleCopySubLoopOnce();
-  } else {
-    RunCacheSampleCopySubLoopOnce();
+void SampleSubLoop() {
+  while (RunSampleSubLoopOnce() && !DistEngine::Get()->ShouldShutdown()) {
   }
+  DistEngine::Get()->ReportThreadFinish();
+}
+
+void DataCopySubLoop() {
+  LoopOnceFunction func;
+  if (!RunConfig::UseGPUCache()) {
+    func = RunDataCopySubLoopOnce;
+  } else {
+    func = RunCacheDataCopySubLoopOnce;
+  }
+
+  while (func() && !DistEngine::Get()->ShouldShutdown()) {
+  }
+
+  DistEngine::Get()->ReportThreadFinish();
+}
+
+void SampleCopySubSloop() {
+  while (true) {
+    if (DistEngine::Get()->ShouldShutdown()) break;
+    RunSampleSubLoopOnce();
+    if (!RunConfig::UseGPUCache()) {
+      RunDataCopySubLoopOnce();
+    } else {
+      RunCacheDataCopySubLoopOnce();
+    }
+  }
+  DistEngine::Get()->ReportThreadFinish();
+}
+
+}  // namespace
+
+void RunArch6LoopsOnce() {
+  RunSampleSubLoopOnce();
+  if (!RunConfig::UseGPUCache()) {
+    RunDataCopySubLoopOnce();
+  } else {
+    RunCacheDataCopySubLoopOnce();
+  }
+}
+
+std::vector<LoopFunction> GetArch6Loops() {
+  std::vector<LoopFunction> func;
+
+  /** make sample & extract overlap or not */
+  // func.push_back(SampleSubLoop);
+  // func.push_back(DataCopySubLoop);
+  func.push_back(SampleCopySubSloop);
+
+  return func;
 }
 
 }  // namespace dist
