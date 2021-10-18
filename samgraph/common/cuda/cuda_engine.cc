5bb2fe320c573ac900f80bd5dfc3e7c6d0cbe335
sxn <xiaoniu.sxn@outlook.com>
2021-10-18 15:30:02 +0800

    minor

--- samgraph/common/cuda/cuda_engine.cc
+++ samgraph/common/cuda/cuda_engine.cc
@@ -303,7 +303,7 @@ void GPUEngine::ArchCheck() {
       CHECK_EQ(_sampler_ctx.device_id, _trainer_ctx.device_id);
       break;
     case kArch3:
-      // CHECK_NE(_sampler_ctx.device_id, _trainer_ctx.device_id);
+      CHECK_NE(_sampler_ctx.device_id, _trainer_ctx.device_id);
       CHECK(!(RunConfig::UseGPUCache() && RunConfig::option_log_node_access));
       break;
     case kArch4:
