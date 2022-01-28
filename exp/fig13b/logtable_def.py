62371f811d157e44f8dd11518a6543f823bd35dc
Jianbang Yang <yangjianbang112@gmail.com>
2022-01-27 22:12:43 -0800

    finish fig13b

---
+++ exp/fig13b/logtable_def.py
@@ -0,0 +1,144 @@
+from common import *
+from common.runner2 import *
+
+
+def get_dgl_logtable():
+    return LogTable(
+        num_row=8,
+        num_col=1
+    ).update_col_definition(
+        col_id=0,
+        definition='epoch_time:total'
+    ).update_row_definition(
+        row_id=0,
+        col_range=[0, 0],
+        num_worker=1
+    ).update_row_definition(
+        row_id=1,
+        col_range=[0, 0],
+        num_worker=2
+    ).update_row_definition(
+        row_id=2,
+        col_range=[0, 0],
+        num_worker=3
+    ).update_row_definition(
+        row_id=3,
+        col_range=[0, 0],
+        num_worker=4
+    ).update_row_definition(
+        row_id=4,
+        col_range=[0, 0],
+        num_worker=5
+    ).update_row_definition(
+        row_id=5,
+        col_range=[0, 0],
+        num_worker=6
+    ).update_row_definition(
+        row_id=6,
+        col_range=[0, 0],
+        num_worker=7
+    ).update_row_definition(
+        row_id=7,
+        col_range=[0, 0],
+        num_worker=8
+    ).create()
+
+
+def get_fgnn_logtable():
+    return LogTable(
+        num_row=18,
+        num_col=1
+    ).update_col_definition(
+        col_id=0,
+        definition='pipeline_train_epoch_time'
+    ).update_row_definition(
+        row_id=0,
+        col_range=[0, 0],
+        num_sample_worker=1,
+        num_train_worker=1
+    ).update_row_definition(
+        row_id=1,
+        col_range=[0, 0],
+        num_sample_worker=1,
+        num_train_worker=2
+    ).update_row_definition(
+        row_id=2,
+        col_range=[0, 0],
+        num_sample_worker=1,
+        num_train_worker=3
+    ).update_row_definition(
+        row_id=3,
+        col_range=[0, 0],
+        num_sample_worker=1,
+        num_train_worker=4
+    ).update_row_definition(
+        row_id=4,
+        col_range=[0, 0],
+        num_sample_worker=1,
+        num_train_worker=5
+    ).update_row_definition(
+        row_id=5,
+        col_range=[0, 0],
+        num_sample_worker=1,
+        num_train_worker=6
+    ).update_row_definition(
+        row_id=6,
+        col_range=[0, 0],
+        num_sample_worker=1,
+        num_train_worker=7
+    ).update_row_definition(
+        row_id=7,
+        col_range=[0, 0],
+        num_sample_worker=2,
+        num_train_worker=1
+    ).update_row_definition(
+        row_id=8,
+        col_range=[0, 0],
+        num_sample_worker=2,
+        num_train_worker=2
+    ).update_row_definition(
+        row_id=9,
+        col_range=[0, 0],
+        num_sample_worker=2,
+        num_train_worker=3
+    ).update_row_definition(
+        row_id=10,
+        col_range=[0, 0],
+        num_sample_worker=2,
+        num_train_worker=4
+    ).update_row_definition(
+        row_id=11,
+        col_range=[0, 0],
+        num_sample_worker=2,
+        num_train_worker=5
+    ).update_row_definition(
+        row_id=12,
+        col_range=[0, 0],
+        num_sample_worker=2,
+        num_train_worker=6
+    ).update_row_definition(
+        row_id=13,
+        col_range=[0, 0],
+        num_sample_worker=3,
+        num_train_worker=1
+    ).update_row_definition(
+        row_id=14,
+        col_range=[0, 0],
+        num_sample_worker=3,
+        num_train_worker=2
+    ).update_row_definition(
+        row_id=15,
+        col_range=[0, 0],
+        num_sample_worker=3,
+        num_train_worker=3
+    ).update_row_definition(
+        row_id=16,
+        col_range=[0, 0],
+        num_sample_worker=3,
+        num_train_worker=4
+    ).update_row_definition(
+        row_id=17,
+        col_range=[0, 0],
+        num_sample_worker=3,
+        num_train_worker=5
+    ).create()
--- exp/fig13b/run.py
+++ exp/fig13b/run.py
@@ -12,7 +12,7 @@ NUM_EPOCH = 3
 TIMESTAMP = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 
 HERE = os.path.abspath(os.path.dirname(__file__))
-DGL_APP_DIR = os.path.join(HERE, '../../example/dgl/multi_gpu')
+DGL_APP_DIR = os.path.join(HERE, '../../example/samgraph/sgnn_dgl')
 FGNN_APP_DIR = os.path.join(HERE, '../../example/samgraph/multi_gpu')
 
 OUTPUT_DIR = os.path.join(HERE, f'output_{TIMESTAMP}')
@@ -21,10 +21,10 @@ def DGL_LOG_DIR(): return os.path.join(OUTPUT_DIR, 'logs_dgl')
 def FGNN_LOG_DIR(): return os.path.join(OUTPUT_DIR, 'logs_fgnn')
 
 
-GNUPLOT_FILE = os.path.join(HERE, 'scale-gcn.plt')
-def OUT_DATA_FILE(): return os.path.join(OUTPUT_DIR, 'fig13a.res')
-def OUT_DATA_FILE_FULL(): return os.path.join(OUTPUT_DIR, 'fig13a-full.res')
-def OUT_FIGURE_FILE(): return os.path.join(OUTPUT_DIR, 'fig13a.eps')
+GNUPLOT_FILE = os.path.join(HERE, 'scale-pinsage.plt')
+def OUT_DATA_FILE(): return os.path.join(OUTPUT_DIR, 'fig13b.res')
+def OUT_DATA_FILE_FULL(): return os.path.join(OUTPUT_DIR, 'fig13b-full.res')
+def OUT_FIGURE_FILE(): return os.path.join(OUTPUT_DIR, 'fig13b.eps')
 
 
 def global_config(args):
@@ -45,29 +45,22 @@ def dgl_scalability_test():
     logtable = get_dgl_logtable()
 
     configs = ConfigList(
-        'DGL  GCN Scalability Test'
+        'DGL  PinSAGE Scalability Test'
     ).select(
         'app',
-        [App.gcn]
+        [App.pinsage]
     ).select(
         'dataset',
         [Dataset.papers100M]
     ).override(
-        'fanout',
-        ['5 10 15']
-    ).override(
         'num_epoch',
         [NUM_EPOCH]
     ).override(
-        'devices',
-        ['0', '0 1', '0 1 2', '0 1 2 3', '0 1 2 3 4',
-            '0 1 2 3 4 5', '0 1 2 3 4 5 6', '0 1 2 3 4 5 6 7'],
+        'num_worker',
+        [1, 2, 3, 4, 5, 6, 7, 8],
     ).override(
-        'BOOL_pipelining',
-        ['pipelining']
-    ).override(
-        'BOOL_use_gpu_sampling',
-        ['use_gpu_sampling']
+        'BOOL_pipeline',
+        ['pipeline']
         # ).override(
         #     'BOOL_validate_configs',
         #     ['validate_configs']
@@ -87,67 +80,62 @@ def fgnn_scalability_test():
     logtable = get_fgnn_logtable()
 
     configs = ConfigList(
-        'FGNN GCN Scalability Test'
+        'FGNN PinSAGE Scalability Test'
     ).select(
         'app',
-        [App.gcn]
+        [App.pinsage]
     ).select(
         'dataset',
         [Dataset.papers100M]
     ).override(
         'sample_type',
-        ['khop2']
+        ['random_walk']
     ).override(
         'num_epoch',
         [NUM_EPOCH]
     ).override(
         'omp-thread-num',
         [40]
-    ).combo(
-        'app',
-        [App.gcn],
-        'fanout',
-        ['5 10 15']
     ).multi_combo_multi_override_list(
         'and',
-        {'app': [App.gcn]},
+        {'app': [App.pinsage]},
         [
             {'num_sample_worker': 1, 'num_train_worker': 1,
-                'BOOL_pipeline': 'pipeline', 'cache_percentage': 0.20},
+                'BOOL_pipeline': 'pipeline', 'cache_percentage': 0.21},
             {'num_sample_worker': 1, 'num_train_worker': 2,
-                'BOOL_pipeline': 'pipeline', 'cache_percentage': 0.20},
+                'BOOL_pipeline': 'pipeline', 'cache_percentage': 0.21},
             {'num_sample_worker': 1, 'num_train_worker': 3,
-                'BOOL_pipeline': 'pipeline', 'cache_percentage': 0.20},
+                'BOOL_pipeline': 'pipeline', 'cache_percentage': 0.21},
             {'num_sample_worker': 1, 'num_train_worker': 4,
-                'BOOL_pipeline': 'pipeline', 'cache_percentage': 0.20},
+                'BOOL_pipeline': 'pipeline', 'cache_percentage': 0.21},
             {'num_sample_worker': 1, 'num_train_worker': 5,
-                'BOOL_pipeline': 'pipeline', 'cache_percentage': 0.20},
+                'BOOL_pipeline': 'pipeline', 'cache_percentage': 0.21},
             {'num_sample_worker': 1, 'num_train_worker': 6,
-                'BOOL_pipeline': 'pipeline', 'cache_percentage': 0.20},
+                'BOOL_pipeline': 'pipeline', 'cache_percentage': 0.21},
             {'num_sample_worker': 1, 'num_train_worker': 7,
-                'BOOL_pipeline': 'pipeline', 'cache_percentage': 0.20},
+                'BOOL_pipeline': 'pipeline', 'cache_percentage': 0.21},
             {'num_sample_worker': 2, 'num_train_worker': 1,
-                'BOOL_pipeline': 'pipeline', 'cache_percentage': 0.20},
+                'BOOL_pipeline': 'pipeline', 'cache_percentage': 0.21},
             {'num_sample_worker': 2, 'num_train_worker': 2,
-                'BOOL_pipeline': 'pipeline', 'cache_percentage': 0.20},
+                'BOOL_pipeline': 'pipeline', 'cache_percentage': 0.21},
             {'num_sample_worker': 2, 'num_train_worker': 3,
-                'BOOL_pipeline': 'pipeline', 'cache_percentage': 0.20},
+                'BOOL_pipeline': 'pipeline', 'cache_percentage': 0.21},
             {'num_sample_worker': 2, 'num_train_worker': 4,
-                'BOOL_pipeline': 'pipeline', 'cache_percentage': 0.20},
+                'BOOL_pipeline': 'pipeline', 'cache_percentage': 0.21},
             {'num_sample_worker': 2, 'num_train_worker': 5,
-                'BOOL_pipeline': 'pipeline', 'cache_percentage': 0.20},
+                'BOOL_pipeline': 'pipeline', 'cache_percentage': 0.21},
             {'num_sample_worker': 2, 'num_train_worker': 6,
-                'BOOL_pipeline': 'pipeline', 'cache_percentage': 0.20},
+                'BOOL_pipeline': 'pipeline', 'cache_percentage': 0.21},
             {'num_sample_worker': 3, 'num_train_worker': 1,
-                'BOOL_pipeline': 'pipeline', 'cache_percentage': 0.20},
+                'BOOL_pipeline': 'pipeline', 'cache_percentage': 0.21},
             {'num_sample_worker': 3, 'num_train_worker': 2,
-                'BOOL_pipeline': 'pipeline', 'cache_percentage': 0.19},
+                'BOOL_pipeline': 'pipeline', 'cache_percentage': 0.21},
             {'num_sample_worker': 3, 'num_train_worker': 3,
-                'BOOL_pipeline': 'pipeline', 'cache_percentage': 0.19},
+                'BOOL_pipeline': 'pipeline', 'cache_percentage': 0.21},
             {'num_sample_worker': 3, 'num_train_worker': 4,
-                'BOOL_pipeline': 'pipeline', 'cache_percentage': 0.19},
+                'BOOL_pipeline': 'pipeline', 'cache_percentage': 0.21},
             {'num_sample_worker': 3, 'num_train_worker': 5,
-                'BOOL_pipeline': 'pipeline', 'cache_percentage': 0.20},
+                'BOOL_pipeline': 'pipeline', 'cache_percentage': 0.21}
         ]
     ).run(
         appdir=FGNN_APP_DIR,
@@ -161,7 +149,7 @@ def fgnn_scalability_test():
     return configs, logtable
 
 
-def run_fig13a_tests():
+def run_fig13b_tests():
     os.system(f'mkdir -p {OUTPUT_DIR}')
     table_format = '{:}\t{:}\t{:}\t{:}\t{:}\n'
     table_format_full = '{:}\t{:}\t{:}\t{:}\t{:}\t# {:}\n'
@@ -169,7 +157,7 @@ def run_fig13a_tests():
         f1.write(table_format.format('"GPUs"',
                                      '"DGL"', '"1S"', '"2S"', '"3S"'))
         f2.write(table_format_full.format('"GPUs"',
-                '"DGL"', '"1S"', '"2S"', '"3S"', '""'))
+                                          '"DGL"', '"1S"', '"2S"', '"3S"', '""'))
 
         print(f'Running tests for fig 13a({OUTPUT_DIR_SHORT})...')
         _, dgl_logtable = dgl_scalability_test()
@@ -197,7 +185,7 @@ def run_fig13a_tests():
         for i in range(8):
             f1.write(table_format.format(str(gpus[i]), str(dgl_data[i]), str(
                 fgnn_1s_data[i]), str(fgnn_2s_data[i]), str(fgnn_3s_data[i])))
-            f2.write(table_format.format(str(gpus[i]), str(dgl_data[i]), str(
+            f2.write(table_format_full.format(str(gpus[i]), str(dgl_data[i]), str(
                 fgnn_1s_data[i]), str(fgnn_2s_data[i]), str(fgnn_3s_data[i]), ' '.join(data_refs[i])))
 
     print('Ploting...')
@@ -208,7 +196,7 @@ def run_fig13a_tests():
 
 
 if __name__ == '__main__':
-    argparser = argparse.ArgumentParser("Fig 13a Tests Runner")
+    argparser = argparse.ArgumentParser("Fig 13b Tests Runner")
     argparser.add_argument('--num-epoch', type=int, default=NUM_EPOCH)
     argparser.add_argument('--mock', action='store_true', default=MOCK)
     argparser.add_argument(
@@ -216,4 +204,4 @@ if __name__ == '__main__':
     args = argparser.parse_args()
 
     global_config(args)
-    run_fig13a_tests()
+    run_fig13b_tests()
