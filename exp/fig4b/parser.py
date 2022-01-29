53304b985068878573f0718ea35547e31c02a85c
sxn <xiaoniu.sxn@outlook.com>
2022-01-29 23:34:39 +0800

    minor update on readme

--- exp/fig4b/parser.py
+++ exp/fig4b/parser.py
@@ -15,8 +15,8 @@ if __name__ == '__main__':
   with open(f'data.dat', 'r') as f:
     table = pandas.read_csv(f, sep='\t')
     # by fixing cache size to 5GB, we may calculate performance under different dimension by varying cache rate
-    # 54228 / 128 * new_dim * cache_rate/100 = 5000
-    # thus we have: cache_rate = 5000 * 128 / cache_rate / 54228 * 100
+    # 54228 / 128 * new_dim * cache_rate/100 = 5120
+    # thus we have: cache_rate = 5120 * 128 / cache_rate / 54228 * 100
     table['dim'] = 5120 * 128 / table['cache_percentage'] / 54228 * 100
     table['new_copy_GB'] = (100 - table['hit_percent'])/100 * table['batch_feat_nbytes'] / 128 * table['dim'] / 1024/1024/1024
 
