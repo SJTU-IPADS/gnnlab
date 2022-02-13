68b9ffb53cfc3ac5cb39259c5fdb57132b0caa13
Jianbang Yang <yangjianbang112@gmail.com>
2022-02-13 09:59:36 -0800

    update runner script

--- example/auto_runner/run_dgl.py
+++ example/auto_runner/run_dgl.py
@@ -243,6 +243,82 @@ def breakdown_test(log_folder=None):
     print('breakdown test uses {:.4f} secs'.format(toc - tic))
 
 
+def pinsage_breakdown_test(log_folder=None, mock=False):
+    tic = time.time()
+
+    if log_folder:
+        log_dir = os.path.join(os.path.join(here, f'run-logs/{log_folder}'))
+    else:
+        log_dir = os.path.join(
+            here, f'run-logs/logs_dgl_{datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}')
+
+    log_table = LogTable(
+        num_row=4,
+        num_col=3
+    ).update_col_definition(
+        col_id=0,
+        definition='sample_time'
+    ).update_col_definition(
+        col_id=1,
+        definition='copy_time'
+    ).update_col_definition(
+        col_id=2,
+        definition='train_time'
+    ).update_row_definition(
+        row_id=0,
+        col_range=[0, 2],
+        app=App.pinsage,
+        dataset=Dataset.products
+    ).update_row_definition(
+        row_id=1,
+        col_range=[0, 2],
+        app=App.pinsage,
+        dataset=Dataset.twitter
+    ).update_row_definition(
+        row_id=2,
+        col_range=[0, 2],
+        app=App.pinsage,
+        dataset=Dataset.papers100M
+    ).update_row_definition(
+        row_id=3,
+        col_range=[0, 2],
+        app=App.pinsage,
+        dataset=Dataset.uk_2006_05
+    ).create()
+
+    ConfigList(
+        test_group_name='DGL PinSAGE breakdown test'
+    ).select(
+        'app',
+        [App.pinsage]
+    ).override(
+        'num_epoch',
+        [10]
+    ).override(
+        'BOOL_use_gpu_sampling',
+        ['use_gpu_sampling']
+    ).override(
+        'BOOL_pipelining',
+        ['no_pipelining']
+    ).override(
+        'devices',
+        ['0']
+        # ).override(
+        #     'BOOL_validate_configs',
+        #     ['validate_configs']
+    ).run(
+        appdir=app_dir,
+        logdir=log_dir,
+        mock=mock
+    ).parse_logs(
+        logtable=log_table,
+        logdir=log_dir
+    )
+
+    toc = time.time()
+
+    print('pinsage breakdown test uses {:.4f} secs'.format(toc - tic))
+
 def scalability_test(log_folder=None, mock=False):
     tic = time.time()
 
@@ -392,6 +468,98 @@ def scalability_test(log_folder=None, mock=False):
     print('scalability test uses {:.4f} secs'.format(toc - tic))
 
 
+def scalability_twitter_test(log_folder=None, mock=False):
+    tic = time.time()
+
+    if log_folder:
+        log_dir = os.path.join(os.path.join(here, f'run-logs/{log_folder}'))
+    else:
+        log_dir = os.path.join(
+            here, f'run-logs/logs_dgl_{datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}')
+
+    log_table = LogTable(
+        num_row=8,
+        num_col=1
+    ).update_col_definition(
+        col_id=0,
+        definition='epoch_time'
+    ).update_row_definition(
+        row_id=0,
+        col_range=[0, 0],
+        devices='0',
+    ).update_row_definition(
+        row_id=1,
+        col_range=[0, 0],
+        devices='0 1',
+    ).update_row_definition(
+        row_id=2,
+        col_range=[0, 0],
+        devices='0 1 2',
+    ).update_row_definition(
+        row_id=3,
+        col_range=[0, 0],
+        devices='0 1 2 3',
+    ).update_row_definition(
+        row_id=4,
+        col_range=[0, 0],
+        devices='0 1 2 3 4',
+    ).update_row_definition(
+        row_id=5,
+        col_range=[0, 0],
+        devices='0 1 2 3 4 5',
+    ).update_row_definition(
+        row_id=6,
+        col_range=[0, 0],
+        devices='0 1 2 3 4 5 6',
+    ).update_row_definition(
+        row_id=7,
+        col_range=[0, 0],
+        devices='0 1 2 3 4 5 6 7',
+    ).create()
+
+    ConfigList(
+        test_group_name='DGL Twitter scalability test'
+    ).select(
+        'app',
+        [App.gcn]
+    ).select(
+        'dataset',
+        [Dataset.twitter]
+    ).override(
+        'fanout',
+        ['5 10 15']
+    ).override(
+        'num_epoch',
+        [10]
+    ).override(
+        'devices',
+        ['0', '0 1', '0 1 2', '0 1 2 3', '0 1 2 3 4',
+            '0 1 2 3 4 5', '0 1 2 3 4 5 6', '0 1 2 3 4 5 6 7'],
+    ).override(
+        'BOOL_pipelining',
+        ['pipelining']
+    ).override(
+        'BOOL_use_gpu_sampling',
+        ['use_gpu_sampling']
+        # ).override(
+        #     'BOOL_validate_configs',
+        #     ['validate_configs']
+    ).run(
+        appdir=app_dir,
+        logdir=log_dir,
+        mock=mock
+    ).parse_logs(
+        logtable=log_table,
+        logdir=log_dir,
+        left_wrap='',
+        right_wrap='',
+        sep='\t'
+    )
+
+    toc = time.time()
+    print('scalability twitter test uses {:.4f} secs'.format(toc - tic))
+
+
 def overall_perf_test(log_folder=None):
     tic = time.time()
 
@@ -475,6 +643,76 @@ def overall_perf_test(log_folder=None):
 
     toc = time.time()
     print('DGL overall performance test uses {:.4f} secs'.format(toc - tic))
+
+
+def pinsage_overall_test(log_folder=None, mock=False):
+    tic = time.time()
+
+    if log_folder:
+        log_dir = os.path.join(os.path.join(here, f'run-logs/{log_folder}'))
+    else:
+        log_dir = os.path.join(
+            here, f'run-logs/logs_dgl_{datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}')
+
+    log_table = LogTable(
+        num_row=4,
+        num_col=1
+    ).update_col_definition(
+        col_id=0,
+        definition='epoch_time'
+    ).update_row_definition(
+        row_id=0,
+        col_range=[0, 0],
+        app=App.pinsage,
+        dataset=Dataset.products
+    ).update_row_definition(
+        row_id=1,
+        col_range=[0, 0],
+        app=App.pinsage,
+        dataset=Dataset.twitter
+    ).update_row_definition(
+        row_id=2,
+        col_range=[0, 0],
+        app=App.pinsage,
+        dataset=Dataset.papers100M
+    ).update_row_definition(
+        row_id=3,
+        col_range=[0, 0],
+        app=App.pinsage,
+        dataset=Dataset.uk_2006_05
+    ).create()
+
+    ConfigList(
+        test_group_name='DGL PinSAGE overall performance test'
+    ).select(
+        'app',
+        [App.pinsage]
+    ).override(
+        'num_epoch',
+        [10]
+    ).override(
+        'devices',
+        ['0 1 2 3 4 5 6 7'],
+    ).override(
+        'BOOL_use_gpu_sampling',
+        ['use_gpu_sampling']
+        # ).override(
+        #     'BOOL_validate_configs',
+        #     ['validate_configs']
+    ).override(
+        'BOOL_pipelining',
+        ['pipelining']
+    ).run(
+        appdir=app_dir,
+        logdir=log_dir,
+        mock=mock
+    ).parse_logs(
+        logtable=log_table,
+        logdir=log_dir
+    )
+
+    toc = time.time()
+    print('DGL Pinsage overall performance test uses {:.4f} secs'.format(toc - tic))
 
 
 def cpu_overall_perf_test(log_folder=None):
@@ -647,4 +885,7 @@ if __name__ == '__main__':
     # scalability_pipeline_test(args.log_folder, args.mock)
     # overall_perf_test(args.log_folder)
     # cpu_overall_perf_test(args.log_folder)
-    motivation2_test(args.log_folder, args.mock)
+    # motivation2_test(args.log_folder, args.mock)
+    # scalability_twitter_test(args.log_folder, args.mock)
+    pinsage_breakdown_test(args.log_folder, args.mock)
+    # pinsage_overall_test(args.log_folder, args.mock)
