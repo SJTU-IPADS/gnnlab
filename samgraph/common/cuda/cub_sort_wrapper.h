da98b999363e4e7264a4b75fc75c6cc05bafcbe9
sxn <xiaoniu.sxn@outlook.com>
2022-06-19 19:43:40 +0800

    add wrapper to cub key sort

--- samgraph/common/cuda/cub_sort_wrapper.h
+++ samgraph/common/cuda/cub_sort_wrapper.h
@@ -8,6 +8,112 @@ namespace samgraph{
 namespace common{
 namespace cuda{
 
+template<typename NativeKey_t>
+void CubSortKey(
+    NativeKey_t* & key, NativeKey_t* & key_alter,
+    const size_t len, Context gpu_ctx, 
+    int begin_bit = 0, int end_bit = sizeof(NativeKey_t) * 8,
+    StreamHandle stream = nullptr) {
+  auto cu_stream = static_cast<cudaStream_t>(stream);
+  auto device = Device::Get(gpu_ctx);
+
+  cub::DoubleBuffer<NativeKey_t> keys(key, key_alter);
+
+  size_t workspace_bytes;
+  void * workspace = nullptr;
+  CUDA_CALL(cub::DeviceRadixSort::SortKeys(
+      workspace, workspace_bytes, keys, len,
+      begin_bit, end_bit, cu_stream));
+
+  workspace = device->AllocWorkspace(gpu_ctx, workspace_bytes);
+  CUDA_CALL(cub::DeviceRadixSort::SortKeys(
+      workspace, workspace_bytes, keys, len,
+      begin_bit, end_bit, cu_stream));
+  device->StreamSync(gpu_ctx, stream);
+
+  key = keys.Current();
+  key_alter = keys.Alternate();
+
+  device->FreeWorkspace(gpu_ctx, workspace);
+}
+
+template<typename NativeKey_t>
+void CubSortKey(
+    const NativeKey_t* key_in, NativeKey_t* key_out,
+    const size_t num_nodes, Context gpu_ctx,
+    int begin_bit = 0, int end_bit = sizeof(NativeKey_t) * 8,
+    StreamHandle stream=nullptr) {
+  auto cu_stream = static_cast<cudaStream_t>(stream);
+  auto device = Device::Get(gpu_ctx);
+
+  size_t workspace_bytes;
+  void *workspace = nullptr;
+  CUDA_CALL(cub::DeviceRadixSort::SortKeys(
+      workspace, workspace_bytes, key_in, key_out, num_nodes,
+      begin_bit, end_bit, cu_stream));
+
+  workspace = device->AllocWorkspace(gpu_ctx, workspace_bytes);
+  CUDA_CALL(cub::DeviceRadixSort::SortKeys(
+      workspace, workspace_bytes, key_in, key_out, num_nodes,
+      begin_bit, end_bit, cu_stream));
+  device->StreamSync(gpu_ctx, stream);
+
+  device->FreeWorkspace(gpu_ctx, workspace);
+}
+
+template<typename NativeKey_t>
+void CubSortKeyDescending(
+    NativeKey_t* & key, NativeKey_t* & key_alter,
+    const size_t len, Context gpu_ctx, 
+    int begin_bit = 0, int end_bit = sizeof(NativeKey_t) * 8,
+    StreamHandle stream = nullptr) {
+  auto cu_stream = static_cast<cudaStream_t>(stream);
+  auto device = Device::Get(gpu_ctx);
+
+  cub::DoubleBuffer<NativeKey_t> keys(key, key_alter);
+
+  size_t workspace_bytes;
+  void * workspace = nullptr;
+  CUDA_CALL(cub::DeviceRadixSort::SortKeysDescending(
+      workspace, workspace_bytes, keys, len,
+      begin_bit, end_bit, cu_stream));
+
+  workspace = device->AllocWorkspace(gpu_ctx, workspace_bytes);
+  CUDA_CALL(cub::DeviceRadixSort::SortKeysDescending(
+      workspace, workspace_bytes, keys, len,
+      begin_bit, end_bit, cu_stream));
+  device->StreamSync(gpu_ctx, stream);
+
+  key = keys.Current();
+  key_alter = keys.Alternate();
+
+  device->FreeWorkspace(gpu_ctx, workspace);
+}
+
+template<typename NativeKey_t>
+void CubSortKeyDescending(
+    const NativeKey_t* key_in, NativeKey_t* key_out,
+    const size_t num_nodes, Context gpu_ctx,
+    int begin_bit = 0, int end_bit = sizeof(NativeKey_t) * 8,
+    StreamHandle stream=nullptr) {
+  auto cu_stream = static_cast<cudaStream_t>(stream);
+  auto device = Device::Get(gpu_ctx);
+
+  size_t workspace_bytes;
+  void *workspace = nullptr;
+  CUDA_CALL(cub::DeviceRadixSort::SortKeysDescending(
+      workspace, workspace_bytes, key_in, key_out, num_nodes,
+      begin_bit, end_bit, cu_stream));
+
+  workspace = device->AllocWorkspace(gpu_ctx, workspace_bytes);
+  CUDA_CALL(cub::DeviceRadixSort::SortKeysDescending(
+      workspace, workspace_bytes, key_in, key_out, num_nodes,
+      begin_bit, end_bit, cu_stream));
+  device->StreamSync(gpu_ctx, stream);
+
+  device->FreeWorkspace(gpu_ctx, workspace);
+}
+
 template<typename NativeKey_t, typename NativeVal_t>
 void CubSortPair(
     NativeKey_t* & key, NativeKey_t* & key_alter,
