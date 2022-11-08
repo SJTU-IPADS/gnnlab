181f64c9d974c4b64949a98f77ce384f39b38e50
Yiwen Zhang <bessss-zyw@sjtu.edu.cn>
2022-11-08 15:09:08 +0800

    update adapter inferface due to torch version upgrade

--- samgraph/torch/adapter.h
+++ samgraph/torch/adapter.h
@@ -18,8 +18,9 @@
 #ifndef SAMGRAPH_TORCH_OPS_H
 #define SAMGRAPH_TORCH_OPS_H
 
-#include <TH/TH.h>
-#include <THC/THC.h>
+// #include <TH/TH.h>
+// #include <THC/THC.h>
+#include <torch/torch.h>
 
 #include <cstdint>
 
@@ -28,18 +29,18 @@ namespace torch {
 
 extern "C" {
 
-THCudaTensor samgraph_torch_get_graph_feat(uint64_t key);
-THCudaTensor samgraph_torch_get_graph_label(uint64_t key);
-THCudaTensor samgraph_torch_get_graph_row(uint64_t key, int layer_idx);
-THCudaTensor samgraph_torch_get_graph_col(uint64_t key, int layer_idx);
-THCudaTensor samgraph_torch_get_graph_data(uint64_t key, int layer_idx);
-THCudaTensor samgraph_torch_get_unsupervised_graph_row(uint64_t key);
-THCudaTensor samgraph_torch_get_unsupervised_graph_col(uint64_t key);
+::torch::Tensor samgraph_torch_get_graph_feat(uint64_t key);
+::torch::Tensor samgraph_torch_get_graph_label(uint64_t key);
+::torch::Tensor samgraph_torch_get_graph_row(uint64_t key, int layer_idx);
+::torch::Tensor samgraph_torch_get_graph_col(uint64_t key, int layer_idx);
+::torch::Tensor samgraph_torch_get_graph_data(uint64_t key, int layer_idx);
+::torch::Tensor samgraph_torch_get_unsupervised_graph_row(uint64_t key);
+::torch::Tensor samgraph_torch_get_unsupervised_graph_col(uint64_t key);
 
-THTensor samgraph_torch_get_dataset_feat();
-THTensor samgraph_torch_get_dataset_label();
-THCudaTensor samgraph_torch_get_graph_input_nodes(uint64_t key);
-THCudaTensor samgraph_torch_get_graph_output_nodes(uint64_t key);
+::torch::Tensor samgraph_torch_get_dataset_feat();
+::torch::Tensor samgraph_torch_get_dataset_label();
+::torch::Tensor samgraph_torch_get_graph_input_nodes(uint64_t key);
+::torch::Tensor samgraph_torch_get_graph_output_nodes(uint64_t key);
 
 }
 
